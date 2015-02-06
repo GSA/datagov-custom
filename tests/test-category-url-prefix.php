2c8b7ccff6705c03dbd8f58d0b3a729159bc3c48
Dan Olaru <dan.olaru@reisystems.com>
2015-02-06 17:33:20 -0500

    add more unit tests

---
+++ tests/test-category-url-prefix.php
@@ -0,0 +1,34 @@
+<?php
+
+class CategoryUrlPrefixTest extends WP_UnitTestCase {
+
+    function test_is_new_page() {
+
+        $referer = 'http://www.data.gov/wp/wp-admin/post-new.php?post_type=page';
+        $this->assertTrue(datagov_custom_is_new_page($referer));
+   
+        $referer = 'http://dev-datagov.reisys.com/wp/wp-admin/post-new.php';
+        $this->assertFalse(datagov_custom_is_new_page($referer));
+        
+        $referer = 'http://www.data.gov/wp/wp-admin/post.php?post=40739&action=edit';
+        $this->assertFalse(datagov_custom_is_new_page($referer));
+    }
+
+    function test_custom_term_hirarchy() {
+
+        // Climate
+        $term = wp_insert_term('Climate', 'category', array('slug' => 'climate'));
+
+        // Coastal Flooding
+        $args = array('slug' => 'coastalflooding', 'parent_id' => $term['term_id']);
+        $term = wp_insert_term('Coastal Flooding', 'category', $args);
+
+        $child_term    = 'coastalflooding';
+        $term_hirarchy = array(0 => 'coastalflooding', 1 => 'climate');
+    
+        $this->assertEquals($term_hirarchy, datagov_custom_term_hirarchy($child_term));
+
+    }
+
+}
+
