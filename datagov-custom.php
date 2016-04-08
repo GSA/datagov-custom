e138c043a32fe9b34b60afa9d6ec8524f862d85b
Alex Perfilov <alexandr.perfilov@reisystems.com>
2016-04-08 15:51:32 -0400

    frontend done

--- datagov-custom.php
+++ datagov-custom.php
@@ -1038,6 +1038,45 @@ function ckan_count_cron()
     return true;
 }
 
+function add_app_agency_query_var( $vars ){
+    $vars[] = "app_agency";
+    return $vars;
+}
+add_filter( 'query_vars', 'add_app_agency_query_var' );
+
+/**
+ * Recursively get taxonomy hierarchy
+ * http://www.daggerhart.com/wordpress-get-taxonomy-hierarchy-including-children/
+ *
+ * @param string $taxonomy
+ * @param int $parent - parent term id
+ * @return array
+ */
+function get_taxonomy_hierarchy( $taxonomy, $parent = 0 ) {
+    // only 1 taxonomy
+    $taxonomy = is_array( $taxonomy ) ? array_shift( $taxonomy ) : $taxonomy;
+
+    // get all direct decendents of the $parent
+    $terms = get_terms( $taxonomy, array( 'parent' => $parent ) );
+
+    // prepare a new array.  these are the children of $parent
+    // we'll ultimately copy all the $terms into this new array, but only after they
+    // find their own children
+    $children = array();
+
+    // go through all the direct decendents of $parent, and gather their children
+    foreach ( $terms as $term ){
+        // recurse to get the direct decendents of "this" term
+        $term->children = get_taxonomy_hierarchy( $taxonomy, $term->term_id );
+
+        // add the term to our new array
+        $children[ $term->term_id ] = $term;
+    }
+
+    // send the results back to the caller
+    return $children;
+}
+
 add_action('cron_prepopulate_application_agencies', 'datagov_prepopulate_application_agencies');
 add_action('admin_init', 'datagov_prepopulate_application_agencies');
 /**
